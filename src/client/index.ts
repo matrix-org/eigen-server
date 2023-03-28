c3532698edc940469df6ee95a26752b15960ca48
Travis Ralston <travisr@matrix.org>
2023-03-28 14:50:22 -0600

    Simple & terrible client mockup

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,1 +1,37 @@
-console.log("Hello world");
+import inquirer from "inquirer";
+
+const ui = new inquirer.ui.BottomBar();
+
+ui.log.write("--------------------------------------------------");
+ui.log.write("==     LINEARIZED MATRIX DEVELOPMENT CLIENT     ==");
+ui.log.write("--------------------------------------------------");
+ui.log.write("Not for production use. Limited to a single room.");
+ui.log.write("Press Ctrl+C to exit or type '\\q'");
+ui.log.write("--------------------------------------------------");
+ui.log.write("");
+
+let buffer: string = "";
+
+function render() {
+    ui.updateBottomBar("> " + buffer);
+}
+render();
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+process.stdin.on("data", (key: string) => {
+    if (key === "\u0003") {
+        process.exit(0); // Ctrl+C
+    } else if (key === "\u000d") {
+        if (buffer.trim() === "\\q") {
+            process.exit(0);
+        }
+        ui.log.write(buffer); // TODO: Actually send message
+        buffer = "";
+        render();
+        return;
+    }
+    buffer += key;
+    render();
+});
