820318a27ce909ec6158545b2c2577aa65bfb44c
Travis Ralston <travisr@matrix.org>
2023-03-30 17:40:36 -0600

    Rename client-server-api for consistency

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import {KeyStore} from "./KeyStore";
 import {RoomStore} from "./RoomStore";
-import {ClientServerApi} from "./client-server";
+import {ClientServerApi} from "./ClientServerApi";
 import {Runtime} from "./Runtime";
 import {SelfSigningKey} from "./SelfSigningKey";
 
