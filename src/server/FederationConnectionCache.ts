365d9aec54c83caba9e0738d0570ac29c1cea028
Travis Ralston <travisr@matrix.org>
2023-07-20 16:47:58 -0600

    Implement name resolution and TLS
    Fixes https://github.com/matrix-org/eigen-server/issues/14
    Fixes https://github.com/matrix-org/eigen-server/issues/13

--- src/server/FederationConnectionCache.ts
+++ src/server/FederationConnectionCache.ts
@@ -1,3 +1,5 @@
+import * as dns from "dns/promises";
+
 export type FederationUrl = {
     certificateHostname: string;
     httpsUrl: string;
@@ -6,17 +8,45 @@ export type FederationUrl = {
 export class FederationConnectionCache {
     private constructor() {}
 
-    public static async getFederationUrl(domain: string): Promise<FederationUrl> {
-        // TODO: Proper server resolution & discovery
-        // https://github.com/matrix-org/eigen-server/issues/13
+    // TODO: Actually do caching
 
-        // TODO: Support https properly
-        // https://github.com/matrix-org/eigen-server/issues/14
+    public static async getFederationUrl(domain: string, withWellKnown = true): Promise<FederationUrl> {
+        const urlForName = (name: string): string => {
+            if (process.env["ES_INSECURE_HTTP"] === "true") {
+                // noinspection HttpUrlsUsage
+                return `http://${name}`;
+            }
+            return `https://${name}`;
+        };
 
-        if (!domain.includes(":")) {
-            domain = `${domain}:8338`; // XXX: Hardcoded!!
+        // Ugly way to check for a port
+        if (domain.match(/:[0-9]{1,5}$/)) {
+            return {certificateHostname: domain.split(":")[0], httpsUrl: urlForName(domain)};
         }
-        // noinspection HttpUrlsUsage
-        return {certificateHostname: domain, httpsUrl: `http://${domain}`};
+
+        if (withWellKnown) {
+            try {
+                const wk = await fetch(`https://${domain}/.well-known/matrix/server`);
+                if (wk.status === 200) {
+                    const j = await wk.json();
+                    if (typeof j["m.server"] === "string") {
+                        return FederationConnectionCache.getFederationUrl(j["m.server"], false);
+                    }
+                }
+            } catch (e) {
+                // ignore
+            }
+        }
+
+        try {
+            const srv = await dns.resolveSrv(`_matrix._tcp.${domain}`);
+            if (srv.length > 0) {
+                return {certificateHostname: domain, httpsUrl: urlForName(`${srv[0].name}:${srv[0].port}`)};
+            }
+        } catch (e) {
+            // ignore
+        }
+
+        return {certificateHostname: domain, httpsUrl: urlForName(`${domain}:8448`)};
     }
 }
--- src/server/index.ts
+++ src/server/index.ts
@@ -8,6 +8,7 @@ import {FederationServer} from "./FederationServer";
 import bodyParser from "body-parser";
 import {InviteStore} from "./InviteStore";
 import * as sourceMapSupport from "source-map-support";
+import {FederationConnectionCache} from "./FederationConnectionCache";
 
 sourceMapSupport.install();
 
@@ -15,6 +16,27 @@ const port: number = Number(process.env["ES_PORT"] ?? 3000);
 const serverName = `${process.env["ES_HOSTNAME"] || "localhost"}:${port}`;
 const app = express();
 app.use(bodyParser.json());
+
+app.get("/test", async (req, res) => {
+    const name = req.query["name"] as string;
+    const connectionDetails = await FederationConnectionCache.getFederationUrl(name);
+    res.status(200);
+    res.write(JSON.stringify(connectionDetails, null, 2) + "\n\n-----\n\n");
+    try {
+        const result = await (
+            await fetch(`${connectionDetails.httpsUrl}/_matrix/federation/v1/version`, {
+                headers: {
+                    Host: connectionDetails.certificateHostname,
+                },
+            })
+        ).json();
+        res.write(JSON.stringify(result, null, 2));
+    } catch (e) {
+        console.error(e);
+        res.write("<!! HTTP REQUEST FAILED !!>");
+    }
+    res.send();
+});
 
 console.log("Server name: ", serverName);
 
